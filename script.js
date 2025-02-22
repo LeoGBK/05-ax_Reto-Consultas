53863cd021338df1f08d805af0eb9e8b3e652f6f
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-22 20:10:42 +0100

    Link to script file and fixing code in JS file

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 const headers = document.querySelectorAll('header');
 const navegacion = document.querySelector('span');
 const pie = document.querySelector('footer');
-
-console.log(headers);
-console.log(navegacion);
-console.log(pie);
